efdac085db8b7e7aff23c281031d8481fc110181
John De Armas <john.dearmas@rhinosecuritylabs.com>
2025-08-01 11:21:49 -0700

    upgrade llm plugins

--- init.lua
+++ init.lua
@@ -125,125 +125,38 @@ require('lazy').setup({
 
   -- LLM Stuff
   {
-		"yacineMTB/dingllm.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local system_prompt =
-				"You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks"
-			local helpful_prompt = "You are a helpful assistant. What I have sent are my notes so far."
-			local dingllm = require("dingllm")
+    'chottolabs/kznllm.nvim',
+    dependencies = {
+      { 'j-hui/fidget.nvim' },
+    },
+    config = function(self)
+      local presets = require 'kznllm.presets.basic'
 
-			local function handle_open_router_spec_data(data_stream)
-				local success, json = pcall(vim.json.decode, data_stream)
-				if success then
-					if json.choices and json.choices[1] and json.choices[1].text then
-						local content = json.choices[1].text
-						if content then
-							dingllm.write_string_at_cursor(content)
-						end
-					end
-				else
-					print("non json " .. data_stream)
-				end
-			end
+      vim.keymap.set({ 'n', 'v' }, '<leader>M', function()
+        presets.switch_presets(presets.options)
+      end, { desc = 'switch between presets' })
 
-			local function custom_make_openai_spec_curl_args(opts, prompt)
-				local url = opts.url
-				local api_key = opts.api_key_name and os.getenv(opts.api_key_name)
-				local data = {
-					prompt = prompt,
-					model = opts.model,
-					temperature = 0.7,
-					stream = true,
-				}
-				local args = { "-N", "-X", "POST", "-H", "Content-Type: application/json", "-d", vim.json.encode(data) }
-				if api_key then
-					table.insert(args, "-H")
-					table.insert(args, "Authorization: Bearer " .. api_key)
-				end
-				table.insert(args, url)
-				return args
-			end
+      local function invoke_with_opts(opts)
+        return function()
+          local preset = presets.load_selected_preset(presets.options)
+          preset.invoke(opts)
+        end
+      end
 
-			local function llama_405b_base()
-				dingllm.invoke_llm_and_stream_into_editor({
-					url = "https://openrouter.ai/api/v1/chat/completions",
-					model = "meta-llama/llama-3.1-405b",
-					api_key_name = "OPEN_ROUTER_API_KEY",
-					max_tokens = "128",
-					replace = false,
-				}, custom_make_openai_spec_curl_args, handle_open_router_spec_data)
-			end
+      vim.keymap.set({ 'n', 'v' }, '<leader>K', invoke_with_opts { debug = true },
+        { desc = 'Send current selection to LLM debug' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>k', invoke_with_opts { debug = false },
+        { desc = 'Send current selection to LLM llm_fill' })
 
-			local function groq_replace()
-				dingllm.invoke_llm_and_stream_into_editor({
-					url = "https://api.groq.com/openai/v1/chat/completions",
-					model = "llama-3.1-70b-versatile",
-					api_key_name = "GROQ_API_KEY",
-					system_prompt = system_prompt,
-					replace = true,
-				}, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-			end
-
-			local function groq_help()
-				dingllm.invoke_llm_and_stream_into_editor({
-					url = "https://api.groq.com/openai/v1/chat/completions",
-					model = "llama-3.1-70b-versatile",
-					api_key_name = "GROQ_API_KEY",
-					system_prompt = helpful_prompt,
-					replace = false,
-				}, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-			end
-
-			local function llama405b_replace()
-				dingllm.invoke_llm_and_stream_into_editor({
-					url = "https://api.lambdalabs.com/v1/chat/completions",
-					model = "hermes-3-llama-3.1-405b-fp8",
-					api_key_name = "LAMBDA_API_KEY",
-					system_prompt = system_prompt,
-					replace = true,
-				}, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-			end
-
-			local function llama405b_help()
-				dingllm.invoke_llm_and_stream_into_editor({
-					url = "https://api.lambdalabs.com/v1/chat/completions",
-					model = "hermes-3-llama-3.1-405b-fp8",
-					api_key_name = "LAMBDA_API_KEY",
-					system_prompt = helpful_prompt,
-					replace = false,
-				}, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-			end
-
-			local function anthropic_help()
-				dingllm.invoke_llm_and_stream_into_editor({
-					url = "https://api.anthropic.com/v1/messages",
-					model = "claude-3-5-sonnet-20240620",
-					api_key_name = "ANTHROPIC_API_KEY",
-					system_prompt = helpful_prompt,
-					replace = false,
-				}, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
-			end
-
-			local function anthropic_replace()
-				dingllm.invoke_llm_and_stream_into_editor({
-					url = "https://api.anthropic.com/v1/messages",
-					model = "claude-3-5-sonnet-20240620",
-					api_key_name = "ANTHROPIC_API_KEY",
-					system_prompt = system_prompt,
-					replace = true,
-				}, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
-			end
-
-			vim.keymap.set({ "v" }, "<leader>k", groq_replace, { desc = "llm groq" })
-			vim.keymap.set({ "v" }, "<leader>K", groq_help, { desc = "llm groq_help" })
-			vim.keymap.set({ "v" }, "<leader>L", llama405b_help, { desc = "llm llama405b_help" })
-			vim.keymap.set({ "v" }, "<leader>l", llama405b_replace, { desc = "llm llama405b_replace" })
-			vim.keymap.set({ "v" }, "<leader>I", anthropic_help, { desc = "llm anthropic_help" })
-			vim.keymap.set({ "v" }, "<leader>i", anthropic_replace, { desc = "llm anthropic" })
-			vim.keymap.set({ "v" }, "<leader>o", llama_405b_base, { desc = "llama base" })
-		end,
-	},
+      vim.api.nvim_set_keymap('n', '<Esc>', '', {
+        noremap = true,
+        silent = true,
+        callback = function()
+          vim.api.nvim_exec_autocmds('User', { pattern = 'LLM_Escape' })
+        end,
+      })
+    end,
+  },
 
   -- LSP & Completion Engine
   {
@@ -285,6 +198,7 @@ require('lazy').setup({
           'html',
           'cssls',
           'rust_analyzer',
+          'gopls',
           'marksman', -- Markdown LSP
           -- Add other LSPs you use
         },
@@ -510,7 +424,7 @@ require('lazy').setup({
             prompt_title  = "🔍 Buffer Search",
             initial_mode  = "insert",
             -- this is your hardcoded prefix:
-            default_text  = "^* ",
+            default_text  = "* ",
           })
         end,
         desc = "Fuzzy search in current buffer with hardcoded PREFIX",
@@ -647,18 +561,44 @@ require('lazy').setup({
 	  -- use opts = {} for passing setup options
 	  -- this is equivalent to setup({}) function
   },
+  -- {
+  --   "ahmedkhalf/project.nvim",
+  --   config = function()
+  --     require("project_nvim").setup {
+  --       -- your configuration comes here
+  --       -- or leave it empty to use the default settings
+  --       -- refer to the configuration section below
+  --       patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+  --     }
+  --   end
+  -- },
+  -- {"rcarriga/nvim-notify"},
   {
-    "ahmedkhalf/project.nvim",
-    config = function()
-      require("project_nvim").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-        patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
-      }
-    end
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      -- lsp_keymaps = false,
+      -- other options
+    },
+    config = function(lp, opts)
+      require("go").setup(opts)
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require('go.format').goimports()
+        end,
+        group = format_sync_grp,
+      })
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
-  {"rcarriga/nvim-notify"},
   { 'tpope/vim-dispatch' }, -- Load on command
   {'kevinhwang91/nvim-bqf'},
   { 'jdearmas/vim-dispatch-neovim', dependencies = { 'tpope/vim-dispatch' } }, -- Load when dispatch is loaded
@@ -1437,12 +1377,6 @@ map('v', '<leader>zn', ":'<,'>TZNarrow<CR>", {})
 map('n', '<leader>zf', ':TZFocus<CR>', {})
 map('n', '<leader>zm', ':TZMinimalist<CR>', {})
 map('n', '<leader>za', ':TZAtaraxis<CR>', {})
-
--- LLM / DingLLM Mappings (Keep visual mode)
-map('v', '<leader>k', ':lua require("dingllm").invoke_llm_and_stream_into_editor({ url = "https://api.groq.com/openai/v1/chat/completions", model = "llama-3.1-70b-versatile", api_key_name = "GROQ_API_KEY", system_prompt = "You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks", replace = true }, require("dingllm").make_openai_spec_curl_args, require("dingllm").handle_openai_spec_data)<CR>', { desc = 'LLM Groq Replace' })
-map('v', '<leader>K', ':lua require("dingllm").invoke_llm_and_stream_into_editor({ url = "[https://api.groq.com/openai/v1/chat/completions](https://api.groq.com/openai/v1/chat/completions)", model = "llama-3.1-70b-versatile", api_key_name = "GROQ_API_KEY", system_prompt = "You are a helpful assistant. What I have sent are my notes so far.", replace = false }, require("dingllm").make_openai_spec_curl_args, require("dingllm").handle_openai_spec_data)<CR>', { desc = 'LLM Groq Help' })
--- Add other dingllm maps similarly, calling the specific functions defined in its config if preferred, or defining them inline like above.
--- ... (Llama405b, Anthropic, etc.)
 
 -- Orgmode specific mappings
 map('n', '<leader>oa', ':lua require("orgmode").action("org_agenda")<CR>', opts) -- Example Org agenda mapping
