7955b1a4205598b3617be48291c98e7ed9ad25a8
jdearmas <var.mail.john@gmail.com>
2025-10-12 20:51:54 -0700

    roam the streets

--- init.lua
+++ init.lua
@@ -575,6 +575,25 @@ require('lazy').setup({
   -- UI Enhancements & Helpers
   { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', event = 'BufReadPost', config = function() require('ibl').setup() end }, -- Load after buffer read
   { 'folke/which-key.nvim', event = 'VeryLazy', config = function() require('which-key').setup() end }, -- Load very late
+  {
+    "chipsenkbeil/org-roam.nvim",
+    tag = "0.2.0",
+    dependencies = {
+      {
+        "nvim-orgmode/orgmode",
+        tag = "0.7.0",
+      },
+    },
+    config = function()
+      require("org-roam").setup({
+        directory = "~/org",
+        -- optional
+        org_files = {
+          "~/org/*.org",
+        }
+      })
+    end
+  },
     {
     "nvim-orgmode/telescope-orgmode.nvim",
     -- event = "VeryLazy",
